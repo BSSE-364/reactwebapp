72264033c0160dfc792c6d5d05dfac342fee1f49
M Adil <raoa3255@gmail.com>
2020-07-23 00:27:43 -0700

    Updates

---
+++ precache-manifest.74f6e0aca7eaba23143289066aabd004.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69208be6a3591d3a2cb9f552f70a41bd",
+    "url": "/reactEcommerce/index.html"
+  },
+  {
+    "revision": "700ae310b652aa9bd0bb",
+    "url": "/reactEcommerce/static/css/main.c24c60ee.chunk.css"
+  },
+  {
+    "revision": "2695727e859ee6fa3c08",
+    "url": "/reactEcommerce/static/js/2.3204d84d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactEcommerce/static/js/2.3204d84d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1adeced7c55465fad6",
+    "url": "/reactEcommerce/static/js/3.c2091437.chunk.js"
+  },
+  {
+    "revision": "700ae310b652aa9bd0bb",
+    "url": "/reactEcommerce/static/js/main.ec29d74f.chunk.js"
+  },
+  {
+    "revision": "9d8d073b66189c29fcd4",
+    "url": "/reactEcommerce/static/js/runtime-main.0eb2250d.js"
+  },
+  {
+    "revision": "aa06551609761402a629143723983fa5",
+    "url": "/reactEcommerce/static/media/dslr1.aa065516.jpg"
+  },
+  {
+    "revision": "93c556b73d7ed1a4caf3bc13e3c7c673",
+    "url": "/reactEcommerce/static/media/dslr2.93c556b7.jpg"
+  },
+  {
+    "revision": "25893d39a7e64c3859dbc5d2a61b2a49",
+    "url": "/reactEcommerce/static/media/dslr3.25893d39.jpg"
+  },
+  {
+    "revision": "05b8f3fb43ed8977ae2857875f50500e",
+    "url": "/reactEcommerce/static/media/dslr4.05b8f3fb.jpg"
+  },
+  {
+    "revision": "ef2731fed922aefba3565abc3b755232",
+    "url": "/reactEcommerce/static/media/dslr5.ef2731fe.jpg"
+  },
+  {
+    "revision": "258de2b2f8d39f02778bbab9f97ed3d7",
+    "url": "/reactEcommerce/static/media/iphone6.258de2b2.jpg"
+  },
+  {
+    "revision": "9ee2c20f0d3d4282ff16f0af58a69c5c",
+    "url": "/reactEcommerce/static/media/iphone7.9ee2c20f.jpg"
+  },
+  {
+    "revision": "6c93b23fa7309516e6d34254cd2dfa8d",
+    "url": "/reactEcommerce/static/media/iphone8.6c93b23f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactlivewebsite/precache-manifest.5f68d126bd5bbfa7452465f668020458.js"
+  "/reactEcommerce/precache-manifest.74f6e0aca7eaba23143289066aabd004.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactlivewebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactEcommerce/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
