2ac43ea2db686f05a8302db82700d588e6cd3b1d
M Adil <raoa3255@gmail.com>
2020-08-08 10:38:55 +0500

    secondcommit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
 
 import App from './OnlineStoreProject/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL} >
-       <App />
+    <Router history={history} >
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
